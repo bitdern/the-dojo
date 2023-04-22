b47e1a109724f7f7cd3440fba0a865f6cf623a75
aidan.newell <aidan.newell@river.com>
2023-04-22 10:50:22 -0500

    more firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcPdGObQmgnsezWaCMjcOE-Cn9aQWyevc",
+  authDomain: "dojoprojectmanny.firebaseapp.com",
+  projectId: "dojoprojectmanny",
+  storageBucket: "dojoprojectmanny.appspot.com",
+  messagingSenderId: "509452510390",
+  appId: "1:509452510390:web:3ac74a856ad0f8fe1cb217",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp();
+
+export { projectAuth, projectFirestore, timestamp };
