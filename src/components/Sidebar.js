643326a5fb802b3a71da126efdddd2e4aa28dcbc
aidan.newell <acnewell09@gmail.com>
2023-04-25 19:28:00 -0500

    asg

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ import "./Sidebar.css";
 import DashboardIcon from "../assets/dashboard_icon.svg";
 import AddIcon from "../assets/add_icon.svg";
 
+// function
 export default function Sidebar() {
   return (
     <div className="sidebar">
